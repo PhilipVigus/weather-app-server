3f4c850c97932fb506b5b37b27509e371c702680
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-12 14:54:21 +0100

    Fix cors error

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import citiesRouter from "./routes/cities";
 const app = express();
 
 app.use(logger("dev"));
-this._app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/cities", citiesRouter);
