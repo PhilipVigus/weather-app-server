7d0e74f18f589d6615a0651487705814c5034af8
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-12 12:46:16 +0100

    Add missing files from last commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
+import citiesRouter from "./routes/cities";
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/cities", citiesRouter);
 
 export default app;
---
+++ server/routes/cities.js
@@ -0,0 +1,16 @@
+import express from "express";
+import fs from "fs";
+const router = express.Router();
+
+router.get("/:id", (req, res, next) => {
+  if (fs.existsSync(`./server/data/letter-${req.params.id}.json`)) {
+    const data = fs.readFileSync(`./server/data/letter-${req.params.id}.json`);
+    res.status(200).send(JSON.parse(data));
+  } else {
+    res
+      .status(422)
+      .send({ error: "No cities found starting with that letter" });
+  }
+});
+
+export default router;
