59abeb6acea16fae38bdd6d9c5ad223c9c35d1a3
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-17 10:37:11 +0100

    Create location name route

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -2,15 +2,21 @@ import express from "express";
 import fs from "fs";
 const router = express.Router();
 
-router.get("/:id", (req, res, next) => {
-  if (fs.existsSync(`./server/data/letter-${req.params.id}.json`)) {
-    const data = fs.readFileSync(`./server/data/letter-${req.params.id}.json`);
+router.get("/:letter", (req, res, next) => {
+  if (fs.existsSync(`./server/data/letter-${req.params.letter}.json`)) {
+    const data = fs.readFileSync(
+      `./server/data/letter-${req.params.letter}.json`
+    );
     res.status(200).send(JSON.parse(data));
   } else {
     res
       .status(422)
       .send({ error: "No locations found starting with that letter" });
   }
+});
+
+router.get("/names/:id", (req, res, next) => {
+  res.status(200).send();
 });
 
 export default router;
--- server/tests/locationsRoute.test.js
+++ server/tests/locationsRoute.test.js
@@ -2,20 +2,29 @@ import request from "supertest";
 import app from "../app";
 
 describe("locations route", () => {
-  it("returns status 200 when you specify a legitimate starting letter", async () => {
-    const res = await request(app).get("/locations/a");
-    expect(res.status).toBe(200);
+  describe("fetching locations starting with initial letters", () => {
+    it("returns status 200 when you specify a legitimate starting letter", async () => {
+      const res = await request(app).get("/locations/a");
+      expect(res.status).toBe(200);
+    });
+
+    it("returns the locations starting with the specified letter", async () => {
+      const res = await request(app).get("/locations/x");
+      expect(res.body.length).toBe(878);
+      expect(res.body[0].name).toBe("Xagħra, Malta (14.26°, 36.05°)");
+      expect(res.body[877].name).toBe("Xıllı, Azerbaijan (49.10°, 39.43°)");
+    });
+
+    it("returns status 422 when the letter is invalid", async () => {
+      const res = await request(app).get("/locations/id");
+      expect(res.status).toBe(422);
+    });
   });
 
-  it("returns the locations starting with the specified letter", async () => {
-    const res = await request(app).get("/locations/x");
-    expect(res.body.length).toBe(878);
-    expect(res.body[0].name).toBe("Xagħra, Malta (14.26°, 36.05°)");
-    expect(res.body[877].name).toBe("Xıllı, Azerbaijan (49.10°, 39.43°)");
-  });
-
-  it("returns status 422 when the letter is invalid", async () => {
-    const res = await request(app).get("/locations/id");
-    expect(res.status).toBe(422);
+  describe("fetching a single location name for a given id", () => {
+    it("returns status 200 when you specify a legitimate id", async () => {
+      const res = await request(app).get("/locations/names/:id");
+      expect(res.status).toBe(200);
+    });
   });
 });
