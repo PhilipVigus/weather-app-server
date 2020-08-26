640d6abde67e19ec142ca916933c7c878269abd7
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-26 10:42:59 +0100

    Optimise data handling
    Initial data handling has been moved out of the routes themselves,
    meaning that it only happens once rather than each time the route
    is called. This significantly increases response speeds on both
    of the routes the server has.

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -2,12 +2,33 @@ import express from "express";
 import fs from "fs";
 const router = express.Router();
 
+const readSortedFullData = () => {
+  const fullSortedData = fs.readFileSync(`./server/data/sortedFullData.json`);
+  return JSON.parse(fullSortedData);
+};
+
+const readLetterDataFiles = () => {
+  const letterData = {};
+  const dir = fs.opendirSync("./server/data/");
+  let file;
+
+  while ((file = dir.readSync()) !== null) {
+    if (file.name.includes("letter")) {
+      const data = fs.readFileSync(`./server/data/${file.name}`);
+      const parsedData = JSON.parse(data);
+      letterData[file.name] = parsedData;
+    }
+  }
+  dir.closeSync();
+  return letterData;
+};
+
+const fullSortedDataAsJSON = readSortedFullData();
+const fullLetterData = readLetterDataFiles();
+
 router.get("/:letter", (req, res, next) => {
-  if (fs.existsSync(`./server/data/letter-${req.params.letter}.json`)) {
-    const data = fs.readFileSync(
-      `./server/data/letter-${req.params.letter}.json`
-    );
-    res.status(200).send(JSON.parse(data));
+  if (fullLetterData[`letter-${req.params.letter}.json`]) {
+    res.status(200).send(fullLetterData[`letter-${req.params.letter}.json`]);
   } else {
     res
       .status(422)
@@ -16,10 +37,7 @@ router.get("/:letter", (req, res, next) => {
 });
 
 router.get("/names/:id", (req, res, next) => {
-  const data = fs.readFileSync(`./server/data/sortedFullData.json`);
-  const dataAsJSON = JSON.parse(data);
-
-  const location = dataAsJSON.find((location) => {
+  const location = fullSortedDataAsJSON.find((location) => {
     return location.id === parseInt(req.params.id);
   });
 
