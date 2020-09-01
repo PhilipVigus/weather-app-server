2216f62e9a326f48fab9972f522afaa69dd14966
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-01 10:19:22 +0100

    Process a single location correctly

--- server/data/processLocationList.js
+++ server/data/processLocationList.js
@@ -1,13 +1,30 @@
 import fs from "fs";
 
 const processLocationList = (inputFilePath) => {
-  fs.writeFileSync("./server/tests/letter-l.json", "some data", (err) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log("file successfully created");
-    }
+  const data = fs.readFileSync(inputFilePath);
+  const parsedData = JSON.parse(data);
+  console.log(parsedData);
+
+  const processedLocations = parsedData.map((location) => {
+    return {
+      id: location.id,
+      name: "London, United Kingdom (34.33°, 47.15°)",
+    };
   });
+
+  console.log(processedLocations);
+
+  fs.writeFileSync(
+    "./server/tests/letter-l.json",
+    JSON.stringify(processedLocations),
+    (err) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log("file successfully created");
+      }
+    }
+  );
 };
 
 export default processLocationList;
--- server/tests/dataProcessing.test.js
+++ server/tests/dataProcessing.test.js
@@ -1,10 +1,26 @@
 import fs from "fs";
 import processLocationList from "../data/processLocationList.js";
+import e from "express";
 
 describe("processLocationList", () => {
   it("outputs a file with the right filename", () => {
-    processLocationList("./fixtures/singleLocation");
+    processLocationList("./server/tests/fixtures/singleLocation.json");
     expect(fs.existsSync("./server/tests/letter-l.json")).toBe(true);
+    fs.unlinkSync("./server/tests/letter-l.json");
+  });
+
+  it("outputs a file with the right content for a single location", () => {
+    const expectedResult = [
+      {
+        id: 833,
+        name: "London, United Kingdom (34.33°, 47.15°)",
+      },
+    ];
+    processLocationList("./server/tests/fixtures/singleLocation.json");
+
+    const data = fs.readFileSync("./server/tests/letter-l.json");
+    expect(JSON.parse(data)).toEqual(expectedResult);
+
     fs.unlinkSync("./server/tests/letter-l.json");
   });
 });
