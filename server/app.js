305127fe14b33b220bc83d23d5775e50b058a4c9
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-12 10:29:07 +0100

    Remove unused middleware

--- server/app.js
+++ server/app.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
-import logger from "morgan";
-import citiesRouter from "./routes/locations";
+import locationsRouter from "./routes/locations";
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/locations", citiesRouter);
+app.use("/locations", locationsRouter);
 
 export default app;
